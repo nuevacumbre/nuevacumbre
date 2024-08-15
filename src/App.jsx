8dfb02a9a24380a7f76854a4466ce4cb9f48b567
Christopher Espinoza <c.espinoza.ordenes@gmail.com>
2024-08-15 16:49:00 -0400

    se sube primer commit de portafolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Christopher Espinoza",
+  title: "FullStack & Docente Ciberseguridad",
+  email: "c.espinoza@nuevacumbre.cl",
+  gitHub: "nuevacumbre",
+  instagram: "nuevacumbre",
+  linkedIn: "christopher-espinoza-órdenes-1b2b274a/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "nuevacumbre",
+  youTube: "@Nuevacumbre",
 };
 
 const primaryColor = "#4E567E";
